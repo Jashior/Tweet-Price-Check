2dfa95f987c2180a79d3063dbf7f9b07987f2572
saintjuniper <saintjuniper73@gmail.com>
2021-05-14 05:08:04 +0100

    adjustments to invest calculation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,15 @@ import { TwitterHandleService } from './shared/twitter-handle.service';
 })
 export class AppComponent {
   title = 'pricecheck';
-  response;
-
   finalCoinIds: string[] = [];
   mappedSymbols: any = {};
+  allCoinIds: any = [];
   coinValues = [];
-  initialInvestment = 100;
-  investmentValue;
+  initialInvestment: number = 100;
+  totalPercentDifference: number = 0;
 
-  //testing this
-  totalInitialInvestment = 0;
+  twitterPostURL = '';
+  tweetId = '1393021076754599939';
 
   tweet: { date: string; username: string; text: string } = {
     username: '',
@@ -35,13 +34,16 @@ export class AppComponent {
 
   ngOnInit() {
     this.apiService.getAllCoins().subscribe((data) => {
-      data.forEach((coin) => (this.mappedSymbols[coin.symbol] = coin.id));
+      data.forEach((coin) => {
+        this.mappedSymbols[coin.symbol] = coin.id;
+        this.allCoinIds.push(coin.id);
+      });
     });
   }
 
-  getPing() {
-    this.apiService.getPing().subscribe((data) => (this.response = data));
-  }
+  // getPing() {
+  //   this.apiService.getPing().subscribe((data) => (this.response = data));
+  // }
 
   // getPrices() {
   //   this.getPriceAtDate(this.cryptoId, this.date);
@@ -61,17 +63,17 @@ export class AppComponent {
   // }
 
   getTwitterPost() {
-    let id = '1392844458065121285';
+    console.log(`attempting to get twitter post ${this.tweetId}`);
 
-    console.log(`attempting to get twitter post ${id}`);
-    this.twitterService.getTweet(id).subscribe((data) => {
+    // Get tweet id from the twitter post URL
+    this.tweetId = this.twitterPostURL.split('/').pop();
+
+    this.twitterService.getTweet(this.tweetId).subscribe((data) => {
       // Gets the username of the tweeter
       this.tweet.username = data['includes']['users'][0]['username'];
 
       // Gets the text of the tweet
       this.tweet.text = data['data'][0]['text'];
-
-      console.log(`Transforming date ${data['data'][0]['created_at']}`);
 
       // Get the date in 03-05-2020 form
       const datePipe = new DatePipe('en-US');
@@ -80,11 +82,8 @@ export class AppComponent {
         'dd-MM-yyyy'
       );
 
-      console.log(`Detected date as ${this.tweet.date}`);
-
       // Once loaded, analyse tweet
       this.analyseTweet();
-      // this.loadTickers();
     });
   }
 
@@ -96,9 +95,8 @@ export class AppComponent {
     this.getCurrentEvaluation();
   }
 
+  // tweet text -> load ids into finalCoinIds
   loadTickers() {
-    console.log(`Attempting to load tickers from tweet text`);
-
     // Matches all tickers in tweet text e.g. $XRP, $BTC
     const regex = /(^|\s)[\$|\#]([A-Za-z_][A-Za-z0-9_]*)/gm;
     const foundTickers = this.tweet.text.match(regex);
@@ -109,8 +107,14 @@ export class AppComponent {
         .replace(/(^\s+|\s+$)/g, '')
         .substring(1)
         .toLowerCase();
+      // checks ticker usage and maps it to relevant id e.g. #XRP -> 'ripple'
       if (splicedTicker in this.mappedSymbols) {
         this.finalCoinIds.push(this.mappedSymbols[splicedTicker]);
+      } else {
+        // checks if the id exists e.g. usage of #bitcoin -> 'bitcoin'
+        if (this.allCoinIds.includes(splicedTicker)) {
+          this.finalCoinIds.push(splicedTicker);
+        }
       }
     }
 
@@ -124,7 +128,7 @@ export class AppComponent {
     console.log(`Attempting to evaluate difference in value of coins `);
 
     this.coinValues = [];
-    this.investmentValue = 0;
+    this.totalPercentDifference = 0;
 
     let investmentPerCoin = this.initialInvestment / this.finalCoinIds.length;
     // let investmentPerCoin = 100;
@@ -146,25 +150,23 @@ export class AppComponent {
                 // Price at current date
                 let currentCoinPrice =
                   data['market_data']['current_price']['usd'];
-                // Number of coins bought at tweet date (= (investment per coin) / (coin price at tweet date))
-                let coinsBought = investmentPerCoin / oldCoinPrice;
-                // Current value of coins bought (= (coins bought at tweet date)*(current value per coin))
-                let coinsBoughtValue = coinsBought * currentCoinPrice;
+
+                // Percent difference
+                let percentDifference =
+                  100 * (currentCoinPrice / oldCoinPrice) - 100;
 
                 this.coinValues.push({
                   name: coin,
                   priceAtDate: oldCoinPrice,
                   currentPrice: currentCoinPrice,
-                  coinsBought: coinsBought,
-                  coinsBoughtValue: coinsBoughtValue,
-                  difference: 100 * (currentCoinPrice / oldCoinPrice) - 100,
+                  percentDifference: percentDifference,
                 });
 
-                // Tally total value of investment
-                this.investmentValue += coinsBoughtValue;
-
-                // Tally total amount invested
-                this.totalInitialInvestment += investmentPerCoin;
+                // Tally percent differenes
+                this.totalPercentDifference += percentDifference;
+                console.log(
+                  `total percent difference: ${this.totalPercentDifference}`
+                );
               }
             });
           }
